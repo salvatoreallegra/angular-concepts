f344cbf83e5bd65510f975effe152d650fb5d2bc
Salvatore Allegra <salallegra@gmail.com>
2025-08-11 09:24:29 -0700

    complete directives and flow control

--- src/app/features/directives/directives/directives.component.ts
+++ src/app/features/directives/directives/directives.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-directives',
-  imports: [],
   templateUrl: './directives.component.html',
-  styleUrl: './directives.component.css'
 })
 export class DirectivesComponent {
-
+  show = true;
+  items = ['One', 'Two', 'Three'];
+  role: 'admin' | 'user' = 'user';
+  danger = false;
+  toggle() {
+    this.show = !this.show;
+  }
+  flipDanger() {
+    this.danger = !this.danger;
+  }
 }
