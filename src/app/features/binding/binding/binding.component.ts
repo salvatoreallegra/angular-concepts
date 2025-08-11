e027889afc642a2fa02cd994549f401b5cc0cd2e
Salvatore Allegra <salallegra@gmail.com>
2025-08-11 08:26:40 -0700

    Add data binding example

--- src/app/features/binding/binding/binding.component.ts
+++ src/app/features/binding/binding/binding.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './binding.component.html',
-  styleUrl: './binding.component.css'
 })
 export class BindingComponent {
-
+  username = 'Alice';
+  photoUrl =
+    'https://www.thedrive.com/wp-content/uploads/images-by-url-td/content/2021/09/IMG_2919-1.jpg?quality=85';
+  clicks = 0;
+  increment() {
+    this.clicks++;
+  }
 }
