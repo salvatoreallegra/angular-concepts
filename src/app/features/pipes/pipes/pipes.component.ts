b72b333f7a7af8645bec159f908f0ca33f4a6aff
Salvatore Allegra <salallegra@gmail.com>
2025-08-11 17:56:35 -0700

    Complete pipes demo

--- src/app/features/pipes/pipes/pipes.component.ts
+++ src/app/features/pipes/pipes/pipes.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  DatePipe,
+  CurrencyPipe,
+  DecimalPipe,
+  PercentPipe,
+  UpperCasePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  SlicePipe,
+  JsonPipe,
+} from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-pipes',
-  imports: [],
+  imports: [
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    PercentPipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe,
+    SlicePipe,
+    JsonPipe,
+  ],
   templateUrl: './pipes.component.html',
-  styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
-
+  price = 1234.56;
+  ratio = 0.34567;
+  qty = 9876.54321;
+  today = new Date();
+  text = 'hello world from angular';
+  list = ['alpha', 'beta', 'gamma', 'delta', 'epsilon'];
+  obj = { id: 42, name: 'Widget', tags: ['a', 'b'] };
 }
